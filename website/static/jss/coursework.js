1473d119a136427d29bfa749cb12bc1d2b846c03
William Horst <whorst042@gmail.com>
2016-03-29 10:14:26 -0400

    Finished the Coursework section and starting on the projects section

---
+++ website/static/jss/coursework.js
@@ -0,0 +1,136 @@
+
+jQuery(document).ready(function(){
+    // Hide all the divtoggle tags, and show the anchors
+    jQuery ('.divtoggle1').hide();
+    jQuery ('.divtoggle2').hide();
+    jQuery ('.divtoggle3').hide();
+    jQuery ('.divtoggle4').hide();
+    jQuery(".aShow").show();
+
+    jQuery('.ANT320_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle1').hide();
+        jQuery("#ANT320").slideToggle();
+    });
+
+    jQuery('.COM357_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle1').hide();
+        jQuery("#COM357").slideToggle();
+    });
+
+    jQuery('.CPS280S_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle1').hide();
+        jQuery("#CPS280s").slideToggle();
+    });
+
+    jQuery('.CPS360_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle1').hide();
+        jQuery("#CPS360").slideToggle();
+    });
+
+    jQuery('.ENG175WI_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle1').hide();
+        jQuery("#ENG175WI").slideToggle();
+    });
+
+    ////////////////////////////////////////////////////
+
+      jQuery('.BCA211_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle2').hide();
+        jQuery("#BCA211").slideToggle();
+    });
+
+    jQuery('.CPS210_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle2').hide();
+        jQuery("#CPS210").slideToggle();
+    });
+
+    jQuery('.CPS240_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle2').hide();
+        jQuery("#CPS240").slideToggle();
+    });
+
+    jQuery('.ENG201_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle2').hide();
+        jQuery("#ENG201").slideToggle();
+    });
+
+    jQuery('.STA382_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle2').hide();
+        jQuery("#STA382").slideToggle();
+    });
+///////////////////////////////////////////////////////////////
+
+
+    jQuery('.BUS100_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle3').hide();
+        jQuery("#BUS100").slideToggle();
+    });
+
+    jQuery('.CPS181_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle3').hide();
+        jQuery("#CPS181").slideToggle();
+    });
+
+    jQuery('.HDF100_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle3').hide();
+        jQuery("#HDF100").slideToggle();
+    });
+
+    jQuery('.MTH132_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle3').hide();
+        jQuery("#MTH132").slideToggle();
+    });
+
+    jQuery('.PSY100_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle3').hide();
+        jQuery("#PSY100").slideToggle();
+    });
+///////////////////////////////////////////////////////////////
+
+
+        jQuery('.AST111_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle4').hide();
+        jQuery("#AST111").slideToggle();
+    });
+
+    jQuery('.BCA101_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle4').hide();
+        jQuery("#BCA101").slideToggle();
+    });
+
+    jQuery('.CPS180_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle4').hide();
+        jQuery("#CPS180").slideToggle();
+    });
+
+    jQuery('.MTH130_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle4').hide();
+        jQuery("#MTH130").slideToggle();
+    });
+
+    jQuery('.MUS113_T').click(function(){
+    // Hide all the DIVs, and show just the one we want
+        jQuery ('.divtoggle4').hide();
+        jQuery("#MUS113").slideToggle();
+    });
+
+});
